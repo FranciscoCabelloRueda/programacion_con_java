82b767913de064881b4d4dc874621a3fb251e15b
FranciscoCabelloRueda <fcabrue@g.educaand.es>
2023-11-02 18:50:40 +0100

    Ejercicios T04

---
+++ 04_Sentencias_condicionales/ejemplos/Ejemplo01.java
@@ -0,0 +1,23 @@
+/**
+* Ejemplo01
+*
+* @author Francisco Javier Cabello Rueda
+*/
+public class Ejemplo01 {
+  public static void main(String[] args) {
+
+    System.out.print("¿Qué nota has sacado en el último examen? ");
+    String line = System.console().readLine();
+    double nota = Double.parseDouble( line );
+
+    if (nota >= 5) {
+    System.out.println("¡Enhorabuena!, ¡has aprobado!");
+    } else {
+    System.out.println("Lo siento, has suspendido.");
+
+    }
+  }
+}
+
+
+
