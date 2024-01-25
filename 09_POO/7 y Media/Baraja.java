f35fc6ca3a7bef0796afe81873d40284193f3799
FranciscoCabelloRueda <fcabrue@g.educaand.es>
2024-01-25 19:44:04 +0100

    Juego

--- 09_POO/7 y Media/Baraja.java
+++ 09_POO/7 y Media/Baraja.java
@@ -1,30 +1,47 @@
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Random;
 
 class Baraja {
-    private ArrayList<Carta> cartas;
 
+    //Atributos
+    private Carta[] cartas;
+    private static final int NUM_CARTAS = 40;
+    private int index;
+
+    // Constructor
     public Baraja() {
-        cartas = new ArrayList<>();
+
+        cartas = new Carta[NUM_CARTAS];
+
         String[] palos = {"Oros", "Copas", "Espadas", "Bastos"};
         String[] numeros = {"1", "2", "3", "4", "5", "6", "7", "Sota", "Caballo", "Rey"};
 
+        index = 0;
         for (String palo : palos) {
             for (String numero : numeros) {
-                cartas.add(new Carta(palo, numero));
+                cartas[index] = new Carta(palo, numero);
+                index++;
             }
         }
     }
 
+    //Metodos
+
     public void barajar() {
-        Collections.shuffle(cartas);
+        Random rand = new Random();
+        for (int i = cartas.length - 1; i > 0; i--) {
+            int j = rand.nextInt(i + 1);
+            Carta temp = cartas[i];
+            cartas[i] = cartas[j];
+            cartas[j] = temp;
+        }
+        index = 0;
     }
 
-    public Carta repartirCarta() {
-        if (cartas.isEmpty()) {
-            System.out.println("No hay más cartas en la baraja.");
+    public Carta repartir() {
+        if (index < cartas.length) {
+            return cartas[index++];
+        } else { 
             return null;
         }
-        return cartas.remove(0);
     }
 }
--- 09_POO/7 y Media/Jugador.java
+++ 09_POO/7 y Media/Jugador.java
@@ -1,35 +1,3 @@
-class Jugador {
-    private Mano mano;
-    private double saldo;
-    private double apuesta;
+public class Jugador {
 
-    public Jugador(double saldoInicial) {
-        mano = new Mano();
-        saldo = saldoInicial;
-    }
-
-    public void realizarApuesta(double cantidad) {
-        if (cantidad > saldo) {
-            System.out.println("No tienes suficiente saldo para realizar esa apuesta.");
-        } else {
-            apuesta = cantidad;
-            saldo -= apuesta;
-        }
-    }
-
-    public void recibirCarta(Carta carta) {
-        mano.añadirCarta(carta);
-    }
-
-    public Mano getMano() {
-        return mano;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
-
-    public double getApuesta() {
-        return apuesta;
-    }
 }
